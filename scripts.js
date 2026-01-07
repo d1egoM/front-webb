abb196002373a41e483ae8ebe3b7e6bc061946da
dmartinez <dialmy2094@gmail.com>
2026-01-07 16:00:45 -0500

    parallax effect

--- scripts.js
+++ scripts.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
   const annSwiper = new Swiper('.annSwiper', {
-    slidesPerView: 2,
+    slidesPerView: 1,
     spaceBetween: 30,
+    grabCursor: true,
+
     direction: 'horizontal',
     allowTouchMove: true,
     autoplay: {
@@ -10,7 +12,6 @@ document.addEventListener('DOMContentLoaded', function () {
     },
     loop: true,
     speed: 6000,
-
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
@@ -22,5 +23,11 @@ document.addEventListener('DOMContentLoaded', function () {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
+
+    breakpoints: {
+        640: { slidesPerView: 2 },
+        1024: { slidesPerView: 3 },
+        1440: { slidesPerView: 3 }
+    }
   });
 });
